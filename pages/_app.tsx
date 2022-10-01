9f666fb92385a9916a6102b5ff23adc851aff275
Thomas Barrial <thomas.barrial@gmail.com>
2022-10-01 11:52:12 +0200

    Update starter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,8 @@ import "../src/styles/global.css";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import { useMemo, useReducer } from "react";
-import { Provider } from "react-redux";
 import { AppContext, AppContextDefault } from "../src/context/AppContext";
 import appReducer from "../src/context/reducer/reducer";
-import { store } from "../src/components/store";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     const [state, dispatch] = useReducer(appReducer, AppContextDefault);
@@ -13,11 +11,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 
     return (
         <AppContext.Provider value={app}>
-            <Provider store={store}>
-         
-                    <Component {...pageProps} />
-           
-            </Provider>
+            <Component {...pageProps} />
         </AppContext.Provider>
     );
 }
